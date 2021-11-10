114eaba9c0225a9832ef6a4ffa80d4eebc60e400
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-11-10 21:53:38 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Test />
     <div></div>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Test from "./components/NewComponent.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    Test,
   },
 };
 </script>
---
+++ src/components/NewComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>ciao</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "Test",
+};
+</script>
+<style>
+h1 {
+  font-size: 40px;
+  color: cadetblue;
+  font-style: italic;
+}
+</style>
